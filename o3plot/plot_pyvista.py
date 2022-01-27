52f47e6fa1b6addf9cbd3b5376dadb8c3f3f8fd8
millen1m <maxim.millen@gmail.com>
2022-01-28 10:48:55 +1300

    adding 3d plotting using pyvista - refined.

--- o3plot/plot_pyvista.py
+++ o3plot/plot_pyvista.py
@@ -3,7 +3,7 @@ import pyvista as pv
 # from pyvista import set_plot_theme
 # set_plot_theme('document')
 
-# mesh points
+
 def plot_regular_3dgrid(xs, ys, zs, active=None):
     if active is None:
         active = np.ones((len(xs)-1, len(ys)-1, len(zs)-1))
@@ -12,7 +12,7 @@ def plot_regular_3dgrid(xs, ys, zs, active=None):
     zn = (zs[np.newaxis, np.newaxis, :] * np.ones((len(xs), len(ys), len(zs)))).flatten()
     tags = np.arange(len(xn))
     ts = tags.reshape((len(xs), len(ys), len(zs)))
-    all_ele_verts = []
+    all_ele_node_tags = []
     for i in range(len(xs) - 1):
         for j in range(len(ys) - 1):
             for k in range(len(zs) - 1):
@@ -21,11 +21,18 @@ def plot_regular_3dgrid(xs, ys, zs, active=None):
                 ele_node_tags = [ts[i][j][k], ts[i + 1][j][k], ts[i + 1][j + 1][k], ts[i][j + 1][k],
                                  ts[i][j][k + 1], ts[i + 1][j][k + 1], ts[i + 1][j + 1][k + 1], ts[i][j + 1][k + 1],
                                  ]
-                ele_verts = []
-                for tag in ele_node_tags:
-                    ele_verts.append([xn[tag], yn[tag], zn[tag]])
-                ele_verts = np.array(ele_verts)
-                all_ele_verts.append(ele_verts)
+                all_ele_node_tags.append(ele_node_tags)
+    plot_eles3d(all_ele_node_tags, xn, yn, zn)
+
+
+def plot_eles3d(all_ele_node_tags, xn, yn, zn):
+    all_ele_verts = []
+    for ele_node_tags in all_ele_node_tags:
+        ele_verts = []
+        for tag in ele_node_tags:
+            ele_verts.append([xn[tag], yn[tag], zn[tag]])
+        ele_verts = np.array(ele_verts)
+        all_ele_verts.append(ele_verts)
 
     points = np.array(all_ele_verts).reshape((-1, 3))
     cells_hex = np.arange(len(points)).reshape((-1, 8))
